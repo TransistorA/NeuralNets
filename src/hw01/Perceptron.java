add453e07455064f30e74ad6c9728ce345ab295f
Michael Matirko <mjrm001@bucknell.edu>
2017-03-05 17:49:25 -0500

    Perceptron class updated with a new non-default constructor

--- src/hw01/Perceptron.java
+++ src/hw01/Perceptron.java
@@ -15,6 +15,8 @@
  */
 package hw01;
 
+import java.util.Random;
+
 /**
  *
  * @author Michael Matirko and Annan Miao
@@ -33,8 +35,38 @@ public class Perceptron {
      */
     private float[] weightArr;
 
+    /**
+     * This is the target value for that particular perceptron
+     */
     private int target;
     private int[] outputArr;
+
+    /**
+     * The constructor for the perceptron initializes each perceptron with the
+     * inputs passed to it, and random weights (as this constructor does not
+     * take a weight list as an argument.)
+     *
+     * @param inputArr - An array of integers (the input values)
+     * @param outputArr - An output array that will contain the outputs
+     * @param target - the integer target value
+     * @author Michael Matirko
+     *
+     */
+    public Perceptron(int[] inputArr, int[] outputArr, int target) {
+        this(inputArr, new float[1], outputArr, target);
+
+        // If no weightArray is passed in, just assume that it
+        // is supposed to be initialized to random values
+        float[] weightArr = new float[inputArr.length];
+
+        Random randnumObj = new Random();
+
+        for (int i = 0; i < inputArr.length; i++) {
+            // Set the weight to a random float between -.5 and .5
+            weightArr[i] = randnumObj.nextFloat() - (float) 0.5;
+        }
+
+    }
 
     /**
      * The constructor for the perceptron initializes each perceptron with the
@@ -42,6 +74,8 @@ public class Perceptron {
      *
      * @param inputArr - An array of ints (the input values)
      * @param weightArr - An array of floats (the weight values)
+     * @param outputArr - An output array that will contain the outputs
+     * @param target - the integer target value
      * @author Michael Matirko
      *
      */
