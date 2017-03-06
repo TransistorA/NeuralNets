5fbf2a6080fdaa8d2b9209119bc9978adee60bee
Annan Miao <am049@bucknell.edu>
2017-03-05 19:38:33 -0500

    Updated functions

--- src/hw01/NeuralNet.java
+++ src/hw01/NeuralNet.java
@@ -16,10 +16,71 @@
  */
 package hw01;
 
+import static hw01.NeuralNetClient.z;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author Michael Matirko and Annan Miao
  */
 public class NeuralNet {
+
+    public void update(String fileName) throws FileNotFoundException, IOException {
+        ArrayList<Integer> inputs = new ArrayList<>();
+        ArrayList<Integer> outputs = new ArrayList<>();
+        ArrayList<Float> weights = new ArrayList<>();
+
+        BufferedReader br = null;
+        String newLine = "";
+        br = new BufferedReader(new FileReader(fileName));
+
+        while ((newLine = br.readLine()) != null) {
+
+            String[] numbers = newLine.split(",");
+
+            outputs.add(Integer.parseInt(numbers[numbers.length - 1]));
+            for (int i = 0; i < numbers.length - 1; i++) {
+                inputs.add(Integer.parseInt(numbers[i]));
+            }
+        }
+        //System.out.println(inputs.size() / outputs.size() + 1);
+        Random randnumObj = new Random();
+        for (int i = 0; i < inputs.size() / outputs.size() + 1; i++) {
+            weights.add(randnumObj.nextFloat() - (float) 0.5);
+            //randnumObj.nextFloat() - (float) 0.5
+        }
+
+        float result = 0;
+        float error = 0;
+        for (int j = 0; j < outputs.size() - 1; j++) {
+            int inputIndex = j * weights.size();
+            do {
+                for (int i = 0; i < weights.size() - 1; i++) {
+                    if (error > 0) {
+                        weights.set(i + 1,
+                                    weights.get(i + 1) + (float) 0.3 * error);
+                    }
+                    if (error < 0) {
+                        weights.set(i + 1,
+                                    weights.get(i + 1) - (float) 0.3 * error);
+                    }
+                    result += inputs.get(inputIndex + i) * weights.get(
+                            i + 1);
+                }
+                result += weights.get(0);
+                error = outputs.get(j) - z(result);  // target - net
+            } while (error != 0);
+
+        }
+
+        for (int i = 0; i < weights.size(); i++) {
+            System.out.println(weights.get(i));
+        }
+    }
 
 }
--- src/hw01/NeuralNetClient.java
+++ src/hw01/NeuralNetClient.java
@@ -16,12 +16,8 @@
  */
 package hw01;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -76,59 +72,9 @@ public class NeuralNetClient {
      */
     public static void createNewANN() throws FileNotFoundException, IOException {
         System.out.println("Create ANN");
-        //Scanner input = new Scanner(System.in);
+        NeuralNet net = new NeuralNet();
+        net.update("data.csv");
 
-        //File file = Scanner in = new Scanner(file);
-        ArrayList<Integer> inputs = new ArrayList<>();
-        ArrayList<Integer> outputs = new ArrayList<>();
-        ArrayList<Float> weights = new ArrayList<>();
-
-        BufferedReader br = null;
-        String newLine = "";
-        br = new BufferedReader(new FileReader("data.csv"));
-
-        while ((newLine = br.readLine()) != null) {
-
-            String[] numbers = newLine.split(",");
-
-            outputs.add(Integer.parseInt(numbers[numbers.length - 1]));
-            for (int i = 0; i < numbers.length - 1; i++) {
-                inputs.add(Integer.parseInt(numbers[i]));
-            }
-        }
-        //System.out.println(inputs.size() / outputs.size() + 1);
-        Random randnumObj = new Random();
-        for (int i = 0; i < inputs.size() / outputs.size() + 1; i++) {
-            weights.add(randnumObj.nextFloat() - (float) 0.5);
-            //randnumObj.nextFloat() - (float) 0.5
-        }
-
-        float result = 0;
-        float error = 0;
-        for (int j = 0; j < outputs.size() - 1; j++) {
-            int inputIndex = j * weights.size();
-            do {
-                for (int i = 0; i < weights.size() - 1; i++) {
-                    if (error > 0) {
-                        weights.set(i + 1,
-                                    weights.get(i + 1) + (float) 0.3 * error);
-                    }
-                    if (error < 0) {
-                        weights.set(i + 1,
-                                    weights.get(i + 1) - (float) 0.3 * error);
-                    }
-                    result += inputs.get(inputIndex + i) * weights.get(
-                            i + 1);
-                }
-                result += weights.get(0);
-                error = outputs.get(j) - z(result);  // target - net
-            } while (error != 0);
-
-        }
-
-        for (int i = 0; i < weights.size(); i++) {
-            System.out.println(weights.get(i));
-        }
     }
 
     /**
