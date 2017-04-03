3e6f7e4a6759547e06b2620a3e503d3274dde374
Annan Miao <am049@bucknell.edu>
2017-04-03 01:40:01 -0400

    hw2

---
+++ src/hw02/ActivationFunction.java
@@ -0,0 +1,28 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+*
+* Name: Michael Matirko, Annan Miao
+* Date: Mar 21, 2017
+* Time: 12:08:06 AM
+*
+* Project: csci205_hw
+* Package: hw02
+* File: ActivationFunction
+* Description:
+*
+* ****************************************
+ */
+package hw02;
+
+/**
+ *
+ * @author am049
+ */
+public interface ActivationFunction {
+
+    double activation1(double num);
+
+    double activation2(double num);
+
+}
